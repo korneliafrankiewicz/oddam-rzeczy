e67019a9c1113afa982505ff80d0da39ef86b3ba
korneliafrankiewicz <nelcia6991@gmail.com>
2021-06-11 16:00:34 +0200

    footer done

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import "font-awesome/css/font-awesome.css";
+
+
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -8,8 +10,24 @@ const useStyles = makeStyles((theme) => ({
       position: "absolute",
       bottom: "0",
       textAlign: "center",
-      marginBottom: "20px"
+      marginBottom: "20px",
+      fontSize: "1.2em"
     },
+    footer: {
+      display: "flex",
+      justifyContent: "center"
+    },
+    socialIcons: {
+      position: "absolute",
+      right: "40px",
+      fontSize: "1.5em"
+    },
+    icon: {
+      marginLeft: "15px",
+      "&:hover": {
+        cursor: "pointer",
+    }
+    }
 
     }
     ))
@@ -18,7 +36,16 @@ const Footer = () => {
     const classes = useStyles();
     return ( 
         <div className={classes.root}>
-          Copyright by Coders Lab
+
+          <div className={classes.footer}>
+            Copyright by Coders Lab
+            <div className={classes.socialIcons}>
+              <span className={classes.icon}><i  className="fab fa-facebook-square"></i></span>
+              <span className={classes.icon}><i className="fab fa-instagram"></i></span>
+            </div>
+          </div>
+      
+         
         
         </div>
      );
